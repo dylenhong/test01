9d08f156cc2d4bd7636d757554b8caf3d94bf5c4
dylenhong <38217554+dylenhong@users.noreply.github.com>
2019-02-26 20:23:38 +0800

    Update JDBCUtilTest.java

--- news/test/JDBCUtilTest.java
+++ news/test/JDBCUtilTest.java
@@ -4,7 +4,9 @@ import org.news.util.JDBCUtil;
 import java.sql.Connection;
 
 import static org.junit.Assert.*;
-
+/**
+*JDBC测试
+*/
 public class JDBCUtilTest {
     @Test
     public void getConnection(){
